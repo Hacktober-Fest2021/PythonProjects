d5d2247300275834d1d69091e1ee71c21ecbfd8a
Fardeen Khan <69037256+Fard433n@users.noreply.github.com>
2021-10-29 23:50:55 +0530

    updated advanced scientific calculator
    please consider this and accept pr request

--- Calculator/Calculator.py
+++ Calculator/Calculator.py
@@ -1,108 +1,202 @@
-#Importing the necessary modules
 from tkinter import *
-import parser
-from math import factorial
+import tkinter.messagebox as tmsg
+import os
+import time
+from math import *
 
 
-root = Tk()
-root.title('GUI - Calculator')
+def getvals(event):
+    value = event.widget.cget('text')
+    if value=='Clr':
+        sc_variable.set('')
+    elif value=='=':
+        try:
+            sc_variable.set(eval(screen.get()))
+            screen.update()
+        except Exception as e:
+            sc_variable.set('Error - Wait for 3 sec')
+            screen.update()
+            status_var.set('Preparing...')
+            screen.update()
+            time.sleep(3)
+            sc_variable.set('')
+            screen.update()
+            status_var.set('Ready..')
+            screen.update()
 
-#It keeps the track of current position on the input text field
-i = 0
-# Receives the digit as parameter and display it on the input field
-def get_variables(num):
-    global i
-    display.insert(i,num)
-    i+=1
- 
-# Calculate function scans the string to evaluates and display it
-def calculate():
-    entire_string = display.get()
-    try:
-        a = parser.expr(entire_string).compile()
-        result = eval(a)
-        clear_all()
-        display.insert(0,result)
-    except Exception:
-        clear_all()
-        display.insert(0,"Error")
- 
-# Function which takes operator as input and displays it on the input field
-def get_operation(operator):
-    global i
-    length = len(operator)
-    display.insert(i,operator)
-    i+=length
- 
-#Function to clear the input field 
-def clear_all():
-    display.delete(0,END)
- 
-#Function which works like backspace
-def undo():
-    entire_string = display.get()
-    if len(entire_string):
-        new_string = entire_string[:-1]
-        clear_all()
-        display.insert(0,new_string)
     else:
-        clear_all()
-        display.insert(0,"Error")
+        sc_variable.set(f'{sc_variable.get()}{value}')
 
-#Function to calculate the factorial and display it
-def fact():
-    entire_string = display.get()
-    try:
-        result = factorial(int(entire_string))
-        clear_all()
-        display.insert(0,result)
-    except Exception:
-        clear_all()
-        display.insert(0,"Error")
 
-#--------------------------------------UI Design ---------------------------------------------
 
-#adding the input field
-display = Entry(root)
-display.grid(row=1,columnspan=6,sticky=N+E+W+S)
- 
-#Code to add buttons to the Calculator
+def term_of_use():
+    tmsg.showinfo('Terms of Use ','IF YOU LIVE IN (OR IF YOUR PRINCIPAL PLACE OF BUSINESS IS IN) THE UNITED STATES, PLEASE READ THE BINDING ARBITRATION CLAUSE AND CLASS ACTION WAIVER IN SECTION 11. IT AFFECTS HOW DISPUTES ARE RESOLVED.')
 
-Button(root,text="1",command = lambda :get_variables(1)).grid(row=2,column=0, sticky=N+S+E+W)
-Button(root,text=" 2",command = lambda :get_variables(2)).grid(row=2,column=1, sticky=N+S+E+W)
-Button(root,text=" 3",command = lambda :get_variables(3)).grid(row=2,column=2, sticky=N+S+E+W)
- 
-Button(root,text="4",command = lambda :get_variables(4)).grid(row=3,column=0, sticky=N+S+E+W)
-Button(root,text=" 5",command = lambda :get_variables(5)).grid(row=3,column=1, sticky=N+S+E+W)
-Button(root,text=" 6",command = lambda :get_variables(6)).grid(row=3,column=2, sticky=N+S+E+W)
- 
-Button(root,text="7",command = lambda :get_variables(7)).grid(row=4,column=0, sticky=N+S+E+W)
-Button(root,text=" 8",command = lambda :get_variables(8)).grid(row=4,column=1, sticky=N+S+E+W)
-Button(root,text=" 9",command = lambda :get_variables(9)).grid(row=4,column=2, sticky=N+S+E+W)
- 
-#adding other buttons to the calculator
-Button(root,text="AC",command=lambda :clear_all()).grid(row=5,column=0, sticky=N+S+E+W)
-Button(root,text=" 0",command = lambda :get_variables(0)).grid(row=5,column=1, sticky=N+S+E+W)
-Button(root,text=" .",command=lambda :get_variables(".")).grid(row=5, column=2, sticky=N+S+E+W)
- 
- 
-Button(root,text="+",command= lambda :get_operation("+")).grid(row=2,column=3, sticky=N+S+E+W)
-Button(root,text="-",command= lambda :get_operation("-")).grid(row=3,column=3, sticky=N+S+E+W)
-Button(root,text="*",command= lambda :get_operation("*")).grid(row=4,column=3, sticky=N+S+E+W)
-Button(root,text="/",command= lambda :get_operation("/")).grid(row=5,column=3, sticky=N+S+E+W)
- 
-# adding new operations
-Button(root,text="pi",command= lambda :get_operation("*3.14")).grid(row=2,column=4, sticky=N+S+E+W)
-Button(root,text="%",command= lambda :get_operation("%")).grid(row=3,column=4, sticky=N+S+E+W)
-Button(root,text="(",command= lambda :get_operation("(")).grid(row=4,column=4, sticky=N+S+E+W)
-Button(root,text="exp",command= lambda :get_operation("**")).grid(row=5,column=4, sticky=N+S+E+W)
- 
-Button(root,text="<-",command= lambda :undo()).grid(row=2,column=5, sticky=N+S+E+W)
-Button(root,text="x!", command= lambda: fact()).grid(row=3,column=5, sticky=N+S+E+W)
-Button(root,text=")",command= lambda :get_operation(")")).grid(row=4,column=5, sticky=N+S+E+W)
-Button(root,text="^2",command= lambda :get_operation("**2")).grid(row=5,column=5, sticky=N+S+E+W)
-Button(root,text="^2",command= lambda :get_operation("**2")).grid(row=5,column=5, sticky=N+S+E+W)
-Button(root,text="=",command= lambda :calculate()).grid(columnspan=6, sticky=N+S+E+W)
+def send_feedback():
+    ans=tmsg.askquestion('Feedback Hub','Was your experience good with us ? ')
+    if ans=='yes':
+        tmsg.showinfo('Feedback','Please Rate us on PlayStore')
+    else:
+        tmsg.showinfo('Feedback','We will contact you soon to know about your bad experience')
 
- 
+root=Tk()
+canvas_width=555
+canvas_height=620
+root.geometry(f'{canvas_width}x{canvas_height}')
+root.maxsize(canvas_width,canvas_height)
+root.minsize(canvas_width,canvas_height)
+root.title('CalCulator ')
+root.call('wm', 'iconphoto', root._w, PhotoImage(file='calculator.png'))
+
+
+
+my_menu=Menu(root)
+m1=Menu(my_menu,tearoff=0,fg='red')
+m1.add_command(label='Terms of Use',command=term_of_use)
+m1.add_command(label='Send Feedback',command=send_feedback)
+root.config(menu=my_menu)
+my_menu.add_cascade(label=' About ',menu=m1)
+my_menu.add_command(label='Exit',command=quit)
+
+
+sc_variable=StringVar()
+screen=Entry(root,textvariable=sc_variable,font='lucida 35 bold',fg='black',bg='white',borderwidth=10)
+screen.pack(pady=30)
+
+
+f=Frame(root)
+f.pack()
+b1=Button(f,text='7',font='lucida 15 bold',padx=20,pady=20,borderwidth=3,fg='black',bg='grey',width=3)
+
+b2=Button(f,text='8',font='lucida 15 bold',padx=20,pady=20,borderwidth=3,fg='black',bg='grey',width=3)
+
+b3=Button(f,text='9',font='lucida 15 bold',padx=20,pady=20,borderwidth=3,fg='black',bg='grey',width=3)
+
+b4=Button(f,text='*',font='lucida 15 bold',padx=20,pady=20,borderwidth=3,fg='black',bg='grey',width=3)
+
+b5=Button(f,text='sin',font='lucida 15 bold',padx=20,pady=20,borderwidth=3,fg='black',bg='grey',width=3)
+
+b6=Button(f,text='(',font='lucida 15 bold',padx=20,pady=20,borderwidth=3,fg='black',bg='grey',width=3)
+
+b1.bind('<Button-1>',getvals)
+b2.bind('<Button-1>',getvals)
+b3.bind('<Button-1>',getvals)
+b4.bind('<Button-1>',getvals)
+b5.bind('<Button-1>',getvals)
+b6.bind('<Button-1>',getvals)
+buttons=[b1,b2,b3,b4,b5,b6]
+count=0
+for i in range(6):
+        buttons[count].grid(row=1,column=i)
+        count += 1
+
+
+f=Frame(root)
+f.pack()
+b1=Button(f,text='4',font='lucida 15 bold',padx=20,pady=20,borderwidth=3,fg='black',bg='grey',width=3)
+
+b2=Button(f,text='5',font='lucida 15 bold',padx=20,pady=20,borderwidth=3,fg='black',bg='grey',width=3)
+
+b3=Button(f,text='6',font='lucida 15 bold',padx=20,pady=20,borderwidth=3,fg='black',bg='grey',width=3)
+
+b4=Button(f,text='-',font='lucida 15 bold',padx=20,pady=20,borderwidth=3,fg='black',bg='grey',width=3)
+
+b5=Button(f,text='cos',font='lucida 15 bold',padx=20,pady=20,borderwidth=3,fg='black',bg='grey',width=3)
+
+b6=Button(f,text=')',font='lucida 15 bold',padx=20,pady=20,borderwidth=3,fg='black',bg='grey',width=3)
+
+
+b1.bind('<Button-1>',getvals)
+b2.bind('<Button-1>',getvals)
+b3.bind('<Button-1>',getvals)
+b4.bind('<Button-1>',getvals)
+b5.bind('<Button-1>',getvals)
+b6.bind('<Button-1>',getvals)
+buttons=[b1,b2,b3,b4,b5,b6]
+count=0
+for i in range(6):
+    buttons[count].grid(row=2,column=i)
+    count += 1
+f=Frame(root)
+f.pack()
+b1=Button(f,text='1',font='lucida 15 bold',padx=20,pady=20,borderwidth=3,fg='black',bg='grey',width=3)
+
+b2=Button(f,text='2',font='lucida 15 bold',padx=20,pady=20,borderwidth=3,fg='black',bg='grey',width=3)
+
+b3=Button(f,text='3',font='lucida 15 bold',padx=20,pady=20,borderwidth=3,fg='black',bg='grey',width=3)
+
+b4=Button(f,text='+',font='lucida 15 bold',padx=20,pady=20,borderwidth=3,fg='black',bg='grey',width=3)
+
+b5=Button(f,text='tan',font='lucida 15 bold',padx=20,pady=20,borderwidth=3,fg='black',bg='grey',width=3)
+
+b6=Button(f,text='%',font='lucida 15 bold',padx=20,pady=20,borderwidth=3,fg='black',bg='grey',width=3)
+
+
+b1.bind('<Button-1>',getvals)
+b2.bind('<Button-1>',getvals)
+b3.bind('<Button-1>',getvals)
+b4.bind('<Button-1>',getvals)
+b5.bind('<Button-1>',getvals)
+b6.bind('<Button-1>',getvals)
+buttons=[b1,b2,b3,b4,b5,b6]
+count=0
+for i in range(6):
+    buttons[count].grid(row=3,column=i)
+    count += 1
+f=Frame(root)
+f.pack()
+b1=Button(f,text='.',font='lucida 15 bold',padx=20,pady=20,borderwidth=3,fg='black',bg='grey',width=3)
+
+b2=Button(f,text='0',font='lucida 15 bold',padx=20,pady=20,borderwidth=3,fg='black',bg='grey',width=3)
+
+b3=Button(f,text='sinh',font='lucida 15 bold',padx=20,pady=20,borderwidth=3,fg='black',bg='grey',width=3)
+
+b4=Button(f,text='cosh',font='lucida 15 bold',padx=20,pady=20,borderwidth=3,fg='black',bg='grey',width=3)
+
+b5=Button(f,text='tanh',font='lucida 15 bold',padx=20,pady=20,borderwidth=3,fg='black',bg='grey',width=3)
+
+b6=Button(f,text='pi',font='lucida 15 bold',padx=20,pady=20,borderwidth=3,fg='black',bg='grey',width=3)
+
+b1.bind('<Button-1>',getvals)
+b2.bind('<Button-1>',getvals)
+b3.bind('<Button-1>',getvals)
+b4.bind('<Button-1>',getvals)
+b5.bind('<Button-1>',getvals)
+b6.bind('<Button-1>',getvals)
+buttons=[b1,b2,b3,b4,b5,b6]
+count=0
+for i in range(6):
+    buttons[count].grid(row=4,column=i)
+    count += 1
+f=Frame(root)
+f.pack()
+
+b1=Button(f,text='log10',font='lucida 15 bold',padx=20,pady=20,borderwidth=3,fg='black',bg='grey',width=3)
+
+b2=Button(f,text='exp',font='lucida 15 bold',padx=20,pady=20,borderwidth=3,fg='black',bg='grey',width=3)
+
+b3=Button(f,text='/',font='lucida 15 bold',padx=20,pady=20,borderwidth=3,fg='black',bg='grey',width=3)
+
+b4=Button(f,text='Clr',font='lucida 15 bold',padx=20,pady=20,borderwidth=3,fg='black',bg='grey',width=3)
+
+b5=Button(f,text='log',font='lucida 15 bold',padx=20,pady=20,borderwidth=3,fg='black',bg='grey',width=3)
+
+b6=Button(f,text='=',font='lucida 15 bold',padx=20,pady=20,borderwidth=3,fg='black',bg='grey',width=3)
+
+
+b1.bind('<Button-1>',getvals)
+b2.bind('<Button-1>',getvals)
+b3.bind('<Button-1>',getvals)
+b4.bind('<Button-1>',getvals)
+b5.bind('<Button-1>',getvals)
+b6.bind('<Button-1>',getvals)
+buttons=[b1,b2,b3,b4,b5,b6]
+count=0
+for i in range(6):
+    buttons[count].grid(row=5,column=i)
+    count += 1
+status_var=StringVar()
+status_var.set('Ready..')
+Label(root,textvariable=status_var,relief=SUNKEN,anchor='w',borderwidth=3,bg='yellow',fg='red').pack(side=BOTTOM,fill=X)
+
 root.mainloop()
